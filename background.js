c7ae52b6d6b06f545e8e0b570d88ee43e1e2bae8
ArAnKon <nastulusaart@gmail.com>
2025-11-02 19:23:17 +0300

    Добавлен переход в детали из уведомления

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// Обработчик для создания скриншотов
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type === "CAPTURE_SCREENSHOT") {
         chrome.tabs.captureVisibleTab(null, { format: 'jpeg', quality: 80 }, (dataUrl) => {
@@ -5,8 +6,20 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         });
         return true;
     }
+
+    // Обработчик для открытия истории с конкретной ошибкой
+    if (request.type === "OPEN_HISTORY_WITH_ERROR") {
+        chrome.windows.create({
+            url: chrome.runtime.getURL("history.html"),
+            type: "popup",
+            width: 900,
+            height: 700
+        });
+        return true;
+    }
 });
 
+// Перехват сетевых ошибок
 chrome.webRequest.onCompleted.addListener(
     (details) => {
         if (details.statusCode >= 400) {
